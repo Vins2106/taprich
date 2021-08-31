0c77b7d5383abeac9540ed6c469c1981d309dc6c
Glitch (glitch-hello-node) <none>
2021-08-31 07:27:05 +0000

    📮😻 Checkpoint ./src/assets.js:25276072/123 ./src/views/index.ejs:25276072/799 ./src/public/style.css:25276072/313

--- src/assets.js
+++ src/assets.js
@@ -1,3 +1,4 @@
 module.exports = {
-  taprich: "https://raw.githubusercontent.com/Vins2106/assets/main/taprich.png"
+  taprich: "https://raw.githubusercontent.com/Vins2106/assets/main/taprich.png",
+  question: "https://raw.githubusercontent.com/Vins2106/assets/b131f361985d75088f773a9614a3977c13167386/question.svg"
 }
