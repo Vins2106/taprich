6fc605572a2bc06a6695fbf5c30355a98266f81e
Glitch (glitch-hello-node) <none>
2021-08-31 09:03:05 +0000

    🍅📖 Checkpoint ./src/assets.js:25276072/313 ./src/views/index.ejs:25276072/291

--- src/assets.js
+++ src/assets.js
@@ -1,4 +1,7 @@
 module.exports = {
   taprich: "https://raw.githubusercontent.com/Vins2106/assets/main/taprich.png",
-  question: "https://raw.githubusercontent.com/Vins2106/assets/b131f361985d75088f773a9614a3977c13167386/question.svg"
+  question: "https://raw.githubusercontent.com/Vins2106/assets/b131f361985d75088f773a9614a3977c13167386/question.svg",
+  up: 'https://raw.githubusercontent.com/Vins2106/assets/ea99256d19255822635ad2952e32d0314c456c7a/chevron-up.svg',
+  dollar: 'https://raw.githubusercontent.com/Vins2106/assets/main/dollar-sign.svg',
+  cart: 'https://raw.githubusercontent.com/Vins2106/assets/main/shopping-cart.svg'
 }
