cb450f568b7320c78f396d28f54466395c241de9
Glitch (glitch-hello-node) <none>
2021-08-31 09:27:06 +0000

    🏮🏦 Checkpoint ./src/public/index.css:25276072/4970 ./src/views/index.ejs:25276072/136 ./src/public/script.js:25276072/702 ./src/assets.js:25276072/113

--- src/assets.js
+++ src/assets.js
@@ -2,6 +2,7 @@ module.exports = {
   taprich: "https://raw.githubusercontent.com/Vins2106/assets/main/taprich.png",
   question: "https://raw.githubusercontent.com/Vins2106/assets/b131f361985d75088f773a9614a3977c13167386/question.svg",
   up: 'https://raw.githubusercontent.com/Vins2106/assets/main/chevron-up%20(1).svg',
+  down: 'https://raw.githubusercontent.com/Vins2106/assets/main/chevron-down.svg',
   dollar: 'https://raw.githubusercontent.com/Vins2106/assets/main/dollar-sign.svg',
   cart: 'https://raw.githubusercontent.com/Vins2106/assets/main/shopping-cart.svg'
 }
--- src/public/script.js
+++ src/public/script.js
@@ -1,6 +1,10 @@
 let isFirst = localStorage.getItem("first");
 let fPlay = document.getElementById("first-play")
 let sBtn = document.getElementById("start-btn")
+let Nav = document.getElementById("navbar")
+let navToggle = document.getElementById("nav-toggle")
+let isNav = false;
+
 if (!isFirst) {
   fPlay.style.display = "block";
 }
@@ -8,4 +12,16 @@ if (!isFirst) {
 // btn click
 sBtn.onclick = function() {
   fPlay.classList.add("no-display")
+}
+
+function openNav() {
+  if (isNav) {
+    Nav.classList.remove("navbar-on")
+    isNav = false;
+    navToggle.src = 'https://raw.githubusercontent.com/Vins2106/assets/main/chevron-up%20(1).svg'
+  } else {
+    Nav.classList.add("navbar-on")
+    isNav = true;
+    navToggle.src = 'https://raw.githubusercontent.com/Vins2106/assets/main/chevron-down.svg';
+  }
 }
